c070c809614373299f67fc9432ca876bbdce7ed4
Alex Laursen <ajlaursen@aol.com>
2020-12-04 00:42:28 -0500

    created db schema

---
+++ dbseed.sql
@@ -0,0 +1,11 @@
+USE employeeDB
+
+INSERT INTO department (name)
+VALUES
+    ("Human Resources"),
+    ("IT"),
+    ("Accounting"),
+    ("Marketing"),
+    ("Research and Development")
+
+-- INSERT INTO role(title, )
