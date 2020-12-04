1a9fb8e143831a36b35fb8e691dddf233b02ce54
Alex Laursen <ajlaursen@aol.com>
2020-12-04 14:47:37 -0500

    updateing gitignore

--- dbseed.sql
+++ dbseed.sql
@@ -1,4 +1,10 @@
-USE employeeDB
+USE employeeDB;
+DELETE FROM department;
+DELETE FROM role;
+DELETE FROM employee;
+
+
+
 
 INSERT INTO department (name)
 VALUES
@@ -6,6 +12,18 @@ VALUES
     ("IT"),
     ("Accounting"),
     ("Marketing"),
-    ("Research and Development")
+    ("Research and Development");
 
--- INSERT INTO role(title, )
+INSERT INTO role(title, salary, department_id)
+VALUES
+    ("Manager", 100000, 1),
+    ("Assitant the manager", 50000, 1),
+    ("Salesman", 50000, 4),
+    ("Receptionist", 50000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+    ("Bob", "starky", 1, null),
+    ("sharon", "babushka", 2, 1);
+
+    
