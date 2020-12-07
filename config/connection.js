1c5b2137caf75a75512969c3c40c54b5d687812b
Alex Laursen <ajlaursen@aol.com>
2020-12-06 20:21:28 -0500

    got server to listen

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require ('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    "employeeDB",
-    "root",
-    "rootpass",
+   process.env.DB_NAME,
+   process.env.DB_USER,
+   process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',
-        port: 8080,
+        port: 3306,
     }
 );
 
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const sequelize = require('./config/connection');
+const express = require("express");
+const sequelize = require("./config/connection");
 
-const app = exprees();
-const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3301;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now Listening"));
+});
